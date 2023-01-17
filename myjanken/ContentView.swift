a5307fb517c99aae248cfa1a7ce2dfa7b10bf0fa
tomo <tomoyama1106@gmail.com>
2023-01-18 01:39:16 +0900

    first commit

--- myjanken/ContentView.swift
+++ myjanken/ContentView.swift
@@ -8,15 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var answerNumber = 0
+        
+        
+        var body: some View {
+            
+            VStack {
+                Spacer()
+                if answerNumber == 0 {
+                    Text("これからじゃんけんをします！")
+                        .padding(.bottom)
+                } else if answerNumber == 1 {
+                    Image("made gu")
+                        .resizable()
+                        .scaledToFit()
+                        
+                    Text("グー").padding(.bottom)
+                    
+                    
+                } else if answerNumber == 2 {
+                    Image("choki")
+                        .resizable()
+                        .scaledToFit()
+                    Text("チョキ").padding(.bottom)
+                    
+                } else if answerNumber == 3{
+                    Image("pa")
+                        .resizable()
+                        .scaledToFit()
+                    Text("パー").padding(.bottom)
+                }
+                
+                
+                
+                Button {
+                   var newAnswerNumber = 0
+                   
+                    
+                    repeat {
+                        newAnswerNumber = Int.random(in: 1...4)
+                    } while answerNumber == newAnswerNumber
+                    answerNumber = newAnswerNumber
+                    
+                } label: {
+                    Text("じゃんけんをする！").frame(maxWidth: .infinity)
+                        .frame(height: 100)
+                        .font(.title)
+                        .background(Color.red).foregroundColor(Color.white)
+                    
+                }
+            }
+            
+            
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
