448a01578ea2280bec6e391e75b981e0e178a45f
tomo <tomoyama1106@gmail.com>
2023-01-18 01:37:37 +0900

    Initial Commit

---
+++ myjanken/myjankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  myjankenApp.swift
+//  myjanken
+//
+//  Created by 高橋智也 on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct myjankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
